31a6045ef7ab927260f0c9a9a3f46ed50bf8b647
jrodlm <jarodlmorris@gmail.com>
2025-03-30 21:25:46 -0400

    progress on book routes but 'add' form still not showing - will revisit

--- controllers/books.js
+++ controllers/books.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const User = require('../models/user.js');
 const Book = require('../models/book.js');
-const Club = require('../models/club');
+const Club = require('../models/club.js');
 
 // ALL BOOK ROUTES --- I.N.D.U.C.E.S //
 // INDEX OF BOOKS - WILL SHOW ALL OF THE CLUB'S BOOKS 
@@ -12,7 +12,7 @@ router.get('/', async (req, res) => {
      const allBooks = await Book.find()
      console.log(allBooks)
      res.render('books/index.ejs', {
-         recipes: allBooks 
+         books: allBooks 
      });
     } catch (error) {
       console.log(error)
@@ -23,7 +23,7 @@ router.get('/', async (req, res) => {
 // NEW BOOK FORM - WILL SHOW FORM FOR ADDING A NEW BOOK TO THE 'LIBRARY'
 router.get('/new', async (req, res) => {
     try {
-      const books = await books.find();
+      const books = await Book.find();
       res.render('books/new.ejs', { books });
     } catch (err) {
       console.error('Error adding book:', err);
@@ -40,11 +40,11 @@ router.post('/', async (req, res) => {
         summary: req.body.summary,
         discussionDate: [new Date(req.body.discussionDate)],
         user: req.session.user._id,
-          // I want to include this but have to get the rest of the code figured out first: clubs: [req.session.user.club]
+          // clubs: [req.session.user.club] want to include but must figure out rest of code
       });
   
       await newBook.save();
-      res.redirect('/book');
+      res.redirect('/books');
     } catch (err) {
       console.error('Error adding book:', err);
       res.redirect('/');
--- server.js
+++ server.js
@@ -7,21 +7,28 @@ const methodOverride = require('method-override');
 const morgan = require('morgan');
 const path = require("path");
 const session = require('express-session');
+
+// APP SCHEMA REFERENCES 
 const Book = require('./models/book')
 
+// MIDDLEWARE VARIABLES
 const isSignedIn = require('./middleware/is-signed-in.js');
 const passUserToView = require('./middleware/pass-user-to-view.js');
 
+// CONTROLLER VARIABLES 
 const authController = require('./controllers/auth.js');
+const booksController = require('./controllers/books');
 
+// PORT
 const port = process.env.PORT ? process.env.PORT : '3000';
 
+// DATABASE LINK 
 mongoose.connect(process.env.MONGODB_URI);
-
 mongoose.connection.on('connected', () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
+// EXPRESS MOUNTING OF CONTROLLERS AND MIDDLEWARE 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(morgan('dev'));
@@ -46,7 +53,7 @@ app.get('/', (req, res) => {
 
 app.use('/auth', authController);
 app.use(isSignedIn);
-
+app.use('./books', booksController);
 
 
 app.listen(port, () => {
